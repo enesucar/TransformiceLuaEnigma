853876b13bc85b9cdfadc4342778cb077233f01c
enesucar <50610802+enesucar@users.noreply.github.com>
2022-07-10 17:44:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ $("#submit").click(function () {
     CreateStringForUser(userName)
 });
 
-var a = 11;
+var stick = 11;
 var titleId = 0;
 
 function CreateStringForUser(userName) {
     $.ajax({
-        url: "https://cheese.formice.com/api/players/" + userName,
+        url: "https://cheese.formice.com/api/players/" + userName.replaceAll("#", "-"),
     }).done(function (data) {
-        console.log(data);
         $('#result').text(a.toString() + data.id.toString() + titleId.toString());
-    }).fail(function (data) {
+    }).fail(function () {
         alert("Please enter a nickname.")
     });
+}
+
+String.prototype.replaceAll = function (str1, str2, ignore) {
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, "\\$&"), (ignore ? "gi" : "g")), (typeof (str2) == "string") ? str2.replace(/\$/g, "$$$$") : str2);
 }
